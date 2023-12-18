664ce2249f84a6d8baf5317068fd2e59b4756e47
kanu-ahuja <kanua6706@gmail.com>
2023-12-18 17:59:53 +0530

    index.js small change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 5000
 
 app.get('/get-products',(req,res)=>{
-    res.send("Hi i am here")
+    res.send("Hi i am here dfsdanfasdnfasndfn")
 })
 
 app.listen(port, () => {
